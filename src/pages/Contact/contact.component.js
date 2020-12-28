77015642c5dc61feb70509448fa12085d6749a38
Viktor Dojchinovski <dojcinovski.viktor@gmail.com>
2020-12-28 20:23:56 +0100

    Finish form from scratch

--- src/pages/Contact/contact.component.js
+++ src/pages/Contact/contact.component.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Input from "../../components/UI/Input/input.component";
+
 import "./contact.component.css";
 
 class Contact extends React.Component {
@@ -8,21 +10,72 @@ class Contact extends React.Component {
 
     this.state = {
       contactForm: {
-        firstName: "",
-        email: "",
-        comment: "",
+        firstName: {
+          label: "First Name",
+          element: "input",
+          type: "text",
+          name: "firstName",
+          value: "",
+          valid: false,
+          validation: {
+            required: true,
+            minChars: 2,
+          },
+        },
+        email: {
+          label: "Email",
+          element: "input",
+          type: "email",
+          name: "email",
+          value: "",
+        },
+        comment: {
+          label: "Comment",
+          element: "textarea",
+          name: "comment",
+          value: "",
+        },
       },
     };
   }
 
-  checkValidity(value, rules) {}
+  // Object.entries(this.state.contactForm) --> [[firstName, {...}], [email, {...}], [comment, {...}]]
 
-  onChangeHandler(e) {
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (!rules) {
+      return isValid;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minChars) {
+      isValid = value.trim().length > 1 && isValid;
+    }
+
+    // ...
+
+    return isValid;
+  }
+
+  onChangeHandler(e, name) {
     // deconstruction of the contactForm
     // const updatedContactForm = { firstName: firstName, email: email, comment: comment };
     const updatedContactForm = { ...this.state.contactForm };
 
-    updatedContactForm[e.target.name] = e.target.value;
+    const updatedContactFormElement = { ...updatedContactForm[name] };
+
+    updatedContactFormElement.value = e.target.value;
+
+    updatedContactFormElement.valid = this.checkValidity(
+      updatedContactFormElement.value,
+      updatedContactFormElement.validation
+    );
+
+    updatedContactForm[name] = updatedContactFormElement;
 
     this.setState({ contactForm: updatedContactForm });
   }
@@ -32,35 +85,32 @@ class Contact extends React.Component {
   }
 
   render() {
+    let formElements = Object.entries(this.state.contactForm).map(
+      ([key, value]) => {
+        return {
+          key: key,
+          config: value,
+        };
+      }
+    );
+
     return (
       <div className="container">
         <form onSubmit={this.onSubmit}>
-          <div className="formControl">
-            <label>First Name</label>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.contactForm.firstName}
-              onChange={(e) => this.onChangeHandler(e)}
-            />
-          </div>
-          <div className="formControl">
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.contactForm.email}
-              onChange={(e) => this.onChangeHandler(e)}
-            />
-          </div>
-          <div className="formControl">
-            <label>Comment</label>
-            <textarea
-              name="comment"
-              value={this.state.contactForm.comment}
-              onChange={(e) => this.onChangeHandler(e)}
-            ></textarea>
-          </div>
+          {formElements.map((el) => {
+            return (
+              <Input
+                key={el.key}
+                label={el.config.label}
+                element={el.config.element}
+                type={el.config.type}
+                name={el.config.name}
+                value={el.config.value}
+                valid={el.config.valid}
+                onChangeHandler={(e, name) => this.onChangeHandler(e, name)}
+              />
+            );
+          })}
           <button type="submit">Send</button>
         </form>
       </div>
